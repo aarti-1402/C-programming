76fa5f3f9285621d2cf05f7024ae2b7c58d1e2c1
Aarti Mishra <aartim1402@gmail.com>
2025-02-07 22:01:06 +0530

    even number

---
+++ while.c
@@ -0,0 +1,12 @@
+// Print all even numbers between 1 and 20 using a while loop
+#include<stdio.h>
+void main(){
+    int i=1;
+    while(i<=20){
+        if(i%2==0){
+            printf("%d \n",i);
+        }
+        i++;
+    }
+}
+
